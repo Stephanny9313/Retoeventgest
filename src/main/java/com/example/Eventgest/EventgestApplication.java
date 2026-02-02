8182ea5aaadc04eb2aed4fd3ef960f52b4dde217
Stephanny9313 <bivistephy@gmail.com>
2026-02-01 23:16:03 -0500

    Primer commit: subir proyecto inicial

---
+++ src/main/java/com/example/Eventgest/EventgestApplication.java
@@ -0,0 +1,13 @@
+package com.example.Eventgest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventgestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventgestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Eventgest/EventgestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Eventgest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventgestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
