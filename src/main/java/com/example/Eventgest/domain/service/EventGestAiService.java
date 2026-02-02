ef2154d93436f4158a73a98af16880dbe8548da9
Bivian <bivistephy@gmail.com>
2026-02-02 17:41:28 -0500

    configurar de la ia

--- src/main/java/com/example/Eventgest/domain/service/EventGestAiService.java
+++ src/main/java/com/example/Eventgest/domain/service/EventGestAiService.java
@@ -1,13 +1,12 @@
 package com.example.Eventgest.domain.service;
 
 
-import dev.langchain4j.service.UserMessage;
-import dev.langchain4j.service.spring.AiService;
 
-@AiService
+
+//@AiService
 public interface EventGestAiService {
 
-    @UserMessage("Generate a greeting message for the given name.")
+    //@UserMessage("Generate a greeting message for the given name.")
     String generateGreeting(String name);
 
 }
